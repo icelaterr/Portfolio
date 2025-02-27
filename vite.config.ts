da3fe769de47ee73876b2a4c93a7b5a35ceffdf4
icelater2 <xsincapalvinx@gmail.com>
2025-02-28 01:08:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // İstemci tarafında process.env üzerinden erişmek istiyorsanız
+    'process.env': {
+      VITE_BOT_TOKEN: JSON.stringify(process.env.VITE_BOT_TOKEN || 'default_token_if_not_defined')
+    }
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
