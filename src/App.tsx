ac9b9329b3aef097a5c951a39c52b9b0b804d69d
icelaterr <dalbudakumit2@gmail.com>
2025-02-28 00:14:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
       <Header />
       
       {/* Audio Player */}
-      <AudioPlayer audioSrc="/music/music.mp3" />
+      <AudioPlayer audioSrc="/music/music.mp4" />
       
       {/* Hero Section */}
       <section id="home" className="min-h-screen flex items-center justify-center relative pt-20">
