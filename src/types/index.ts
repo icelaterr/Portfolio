a98dd96fe678ccdf88f20859a6cf9f8f0560e187
icelaterr <dalbudakumit2@gmail.com>
2025-02-27 21:27:26 +0300

    Add files via upload

---
+++ src/types/index.ts
@@ -0,0 +1,53 @@
+export interface DiscordUser {
+  id: string;
+  username: string;
+  avatar: string;
+  discriminator: string;
+  status?: string;
+  activities?: DiscordActivity[];
+  banner?: string;
+  accent_color?: number;
+}
+
+export interface DiscordActivity {
+  name: string;
+  type: number;
+  state?: string;
+  details?: string;
+  timestamps?: {
+    start?: number;
+    end?: number;
+  };
+  assets?: {
+    large_image?: string;
+    large_text?: string;
+    small_image?: string;
+    small_text?: string;
+  };
+}
+
+export interface GitHubRepo {
+  id: number;
+  name: string;
+  full_name: string;
+  html_url: string;
+  description: string;
+  fork: boolean;
+  created_at: string;
+  updated_at: string;
+  pushed_at: string;
+  homepage: string;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  forks_count: number;
+  open_issues_count: number;
+  license: {
+    key: string;
+    name: string;
+    url: string;
+  } | null;
+  topics: string[];
+  visibility: string;
+}
